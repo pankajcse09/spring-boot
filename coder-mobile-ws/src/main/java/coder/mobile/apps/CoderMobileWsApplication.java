8c70eafb2ed58d51a7f2740b877cffabdcd1e385
Pankaj Bharti <Pankaj.Bharti@Capco.com>
2019-12-25 11:42:02 +0700

    Initial Project Setup Spring Boot
    Initial Project Setup Spring Boot

---
+++ coder-mobile-ws/src/main/java/coder/mobile/apps/CoderMobileWsApplication.java
@@ -0,0 +1,13 @@
+package coder.mobile.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderMobileWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderMobileWsApplication.class, args);
+	}
+
+}
